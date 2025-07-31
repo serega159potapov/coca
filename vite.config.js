4be32f82cd9dcda3ea88aafd1e40b088d7871155
Сергей <pyevit@mail.ru>
2025-07-31 12:37:37 +0500

    fix images

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     root: 'src',
-    base: './coca/',
+    base: '/coca/',
     server: {
         port: 3000,
         open: true,
