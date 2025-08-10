79174c27e42b3ed102d0c06a9b855c35fb38b2b3
Сергей <pyevit@mail.ru>
2025-08-10 18:12:53 +0500

    Creating the about page and hero section

---
+++ src/js/about.js
@@ -0,0 +1,10 @@
+import '/scss/about.scss';
+
+//components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useHeroSlider } from './components/about/home.js';
+
+useTheme();
+useBurger();
+useHeroSlider();
