7984926c7cbb01704a886db36dc7b14991b2236f
Сергей <pyevit@mail.ru>
2025-09-04 13:23:42 +0500

    Created detail blog page

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
                 main: './src/index.html',
                 about: './src/about.html',
                 blog: './src/blog.html',
+                detailBlog: './src/detail-blog.html',
                 pricing: './src/pricing.html',
                 contact: './src/contact.html',
             },
